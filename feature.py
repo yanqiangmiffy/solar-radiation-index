ef5209804956bf349e576e9325d0baa0a15a2022
yanqaingmiffy <1185918903@qq.com>
2018-10-21 00:16:57 +0800

    feature poly

--- feature.py
+++ feature.py
@@ -15,7 +15,7 @@ import xgboost as xgb
 import numpy as np
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import KFold
-
+from sklearn.preprocessing import PolynomialFeatures
 
 
 
@@ -82,42 +82,64 @@ def to_one_day(data_df):
         data_df_date.loc[i - 1, '23_气压'] = data.loc[7, '气压']
     return data_df_date
 
+def add_poly_features(data,column_names):
+    features=data[column_names]
+    rest_features=data.drop(column_names,axis=1)
+    poly_transformer=PolynomialFeatures(degree=2,interaction_only=False,include_bias=False)
+    poly_features=pd.DataFrame(poly_transformer.fit_transform(features),columns=poly_transformer.get_feature_names(column_names))
+
+    for col in poly_features.columns:
+        rest_features.insert(1,col,poly_features[col])
+    return rest_features
+
 
 def creat_fea(data_df):
     data_df = to_one_day(data_df)
-    # data_df['风速_mean'] = data_df[['2_风速', '5_风速', '8_风速', '11_风速', '14_风速', '17_风速', '20_风速', '23_风速']].mean(axis=1)
-    # data_df['风速_std'] = data_df[['2_风速', '5_风速', '8_风速', '11_风速', '14_风速', '17_风速', '20_风速', '23_风速']].std(axis=1)
-    # data_df['风速_min'] = data_df[['2_风速', '5_风速', '8_风速', '11_风速', '14_风速', '17_风速', '20_风速', '23_风速']].min(axis=1)
-    # data_df['风速_max'] = data_df[['2_风速', '5_风速', '8_风速', '11_风速', '14_风速', '17_风速', '20_风速', '23_风速']].max(axis=1)
-    #
-    # data_df['辐照度_mean'] = data_df[[ '8_辐照度', '11_辐照度', '14_辐照度', '17_辐照度', '20_辐照度']].mean(
-    #     axis=1)
-    # data_df['辐照度_std'] = data_df[['8_辐照度', '11_辐照度', '14_辐照度', '17_辐照度', '20_辐照度']].std(
-    #     axis=1)
-    # data_df['辐照度_min'] = data_df[['8_辐照度', '11_辐照度', '14_辐照度', '17_辐照度', '20_辐照度']].min(
-    #     axis=1)
-    # data_df['辐照度_max'] = data_df[[ '8_辐照度', '11_辐照度', '14_辐照度', '17_辐照度', '20_辐照度']].max(
-    #     axis=1)
-    #
-    # data_df['风向_mean'] = data_df[['2_风向', '5_风向', '8_风向', '11_风向', '14_风向', '17_风向', '20_风向', '23_风向']].mean(axis=1)
-    # data_df['风向_std'] = data_df[['2_风向', '5_风向', '8_风向', '11_风向', '14_风向', '17_风向', '20_风向', '23_风向']].std(axis=1)
-    # data_df['风向_min'] = data_df[['2_风向', '5_风向', '8_风向', '11_风向', '14_风向', '17_风向', '20_风向', '23_风向']].min(axis=1)
-    # data_df['风向_max'] = data_df[['2_风向', '5_风向', '8_风向', '11_风向', '14_风向', '17_风向', '20_风向', '23_风向']].max(axis=1)
-    #
-    # data_df['温度_mean'] = data_df[['2_温度', '5_温度', '8_温度', '11_温度', '14_温度', '17_温度', '20_温度', '23_温度']].mean(axis=1)
-    # data_df['温度_std'] = data_df[['2_温度', '5_温度', '8_温度', '11_温度', '14_温度', '17_温度', '20_温度', '23_温度']].std(axis=1)
-    # data_df['温度_min'] = data_df[['2_温度', '5_温度', '8_温度', '11_温度', '14_温度', '17_温度', '20_温度', '23_温度']].min(axis=1)
-    # data_df['温度_max'] = data_df[['2_温度', '5_温度', '8_温度', '11_温度', '14_温度', '17_温度', '20_温度', '23_温度']].max(axis=1)
-    #
-    # data_df['湿度_mean'] = data_df[['2_湿度', '5_湿度', '8_湿度', '11_湿度', '14_湿度', '17_湿度', '20_湿度', '23_湿度']].mean(axis=1)
-    # data_df['湿度_std'] = data_df[['2_湿度', '5_湿度', '8_湿度', '11_湿度', '14_湿度', '17_湿度', '20_湿度', '23_湿度']].std(axis=1)
-    # data_df['湿度_min'] = data_df[['2_湿度', '5_湿度', '8_湿度', '11_湿度', '14_湿度', '17_湿度', '20_湿度', '23_湿度']].min(axis=1)
-    # data_df['湿度_max'] = data_df[['2_湿度', '5_湿度', '8_湿度', '11_湿度', '14_湿度', '17_湿度', '20_湿度', '23_湿度']].max(axis=1)
-    #
-    # data_df['气压_mean'] = data_df[['2_气压', '5_气压', '8_气压', '11_气压', '14_气压', '17_气压', '20_气压', '23_气压']].mean(axis=1)
-    # data_df['气压_std'] = data_df[['2_气压', '5_气压', '8_气压', '11_气压', '14_气压', '17_气压', '20_气压', '23_气压']].std(axis=1)
-    # data_df['气压_min'] = data_df[['2_气压', '5_气压', '8_气压', '11_气压', '14_气压', '17_气压', '20_气压', '23_气压']].min(axis=1)
-    # data_df['气压_max'] = data_df[['2_气压', '5_气压', '8_气压', '11_气压', '14_气压', '17_气压', '20_气压', '23_气压']].max(axis=1)
+    data_df['风速_mean'] = data_df[['2_风速', '5_风速', '8_风速', '11_风速', '14_风速', '17_风速', '20_风速', '23_风速']].mean(axis=1)
+    data_df['风速_std'] = data_df[['2_风速', '5_风速', '8_风速', '11_风速', '14_风速', '17_风速', '20_风速', '23_风速']].std(axis=1)
+    data_df['风速_min'] = data_df[['2_风速', '5_风速', '8_风速', '11_风速', '14_风速', '17_风速', '20_风速', '23_风速']].min(axis=1)
+    data_df['风速_max'] = data_df[['2_风速', '5_风速', '8_风速', '11_风速', '14_风速', '17_风速', '20_风速', '23_风速']].max(axis=1)
+
+    data_df['辐照度_mean'] = data_df[[ '8_辐照度', '11_辐照度', '14_辐照度', '17_辐照度', '20_辐照度']].mean(
+        axis=1)
+    data_df['辐照度_std'] = data_df[['8_辐照度', '11_辐照度', '14_辐照度', '17_辐照度', '20_辐照度']].std(
+        axis=1)
+    data_df['辐照度_min'] = data_df[['8_辐照度', '11_辐照度', '14_辐照度', '17_辐照度', '20_辐照度']].min(
+        axis=1)
+    data_df['辐照度_max'] = data_df[[ '8_辐照度', '11_辐照度', '14_辐照度', '17_辐照度', '20_辐照度']].max(
+        axis=1)
+
+    data_df['风向_mean'] = data_df[['2_风向', '5_风向', '8_风向', '11_风向', '14_风向', '17_风向', '20_风向', '23_风向']].mean(axis=1)
+    data_df['风向_std'] = data_df[['2_风向', '5_风向', '8_风向', '11_风向', '14_风向', '17_风向', '20_风向', '23_风向']].std(axis=1)
+    data_df['风向_min'] = data_df[['2_风向', '5_风向', '8_风向', '11_风向', '14_风向', '17_风向', '20_风向', '23_风向']].min(axis=1)
+    data_df['风向_max'] = data_df[['2_风向', '5_风向', '8_风向', '11_风向', '14_风向', '17_风向', '20_风向', '23_风向']].max(axis=1)
+
+    data_df['温度_mean'] = data_df[['2_温度', '5_温度', '8_温度', '11_温度', '14_温度', '17_温度', '20_温度', '23_温度']].mean(axis=1)
+    data_df['温度_std'] = data_df[['2_温度', '5_温度', '8_温度', '11_温度', '14_温度', '17_温度', '20_温度', '23_温度']].std(axis=1)
+    data_df['温度_min'] = data_df[['2_温度', '5_温度', '8_温度', '11_温度', '14_温度', '17_温度', '20_温度', '23_温度']].min(axis=1)
+    data_df['温度_max'] = data_df[['2_温度', '5_温度', '8_温度', '11_温度', '14_温度', '17_温度', '20_温度', '23_温度']].max(axis=1)
+
+    data_df['湿度_mean'] = data_df[['2_湿度', '5_湿度', '8_湿度', '11_湿度', '14_湿度', '17_湿度', '20_湿度', '23_湿度']].mean(axis=1)
+    data_df['湿度_std'] = data_df[['2_湿度', '5_湿度', '8_湿度', '11_湿度', '14_湿度', '17_湿度', '20_湿度', '23_湿度']].std(axis=1)
+    data_df['湿度_min'] = data_df[['2_湿度', '5_湿度', '8_湿度', '11_湿度', '14_湿度', '17_湿度', '20_湿度', '23_湿度']].min(axis=1)
+    data_df['湿度_max'] = data_df[['2_湿度', '5_湿度', '8_湿度', '11_湿度', '14_湿度', '17_湿度', '20_湿度', '23_湿度']].max(axis=1)
+
+    data_df['气压_mean'] = data_df[['2_气压', '5_气压', '8_气压', '11_气压', '14_气压', '17_气压', '20_气压', '23_气压']].mean(axis=1)
+    data_df['气压_std'] = data_df[['2_气压', '5_气压', '8_气压', '11_气压', '14_气压', '17_气压', '20_气压', '23_气压']].std(axis=1)
+    data_df['气压_min'] = data_df[['2_气压', '5_气压', '8_气压', '11_气压', '14_气压', '17_气压', '20_气压', '23_气压']].min(axis=1)
+    data_df['气压_max'] = data_df[['2_气压', '5_气压', '8_气压', '11_气压', '14_气压', '17_气压', '20_气压', '23_气压']].max(axis=1)
+
+    co = ['风速_mean', '辐照度_mean', '风向_mean', '温度_mean', '湿度_mean', '气压_mean']
+
+    data_df = add_poly_features(data_df, co)
+    for i in range(0, len(data_df)):
+        if i < 1:
+            data_df.loc[i, 'old_辐照度_mean'] = data_df.loc[i, '辐照度_mean']
+        elif i < 2:
+            data_df.loc[i, 'old_辐照度_mean'] = (data_df.loc[i, '辐照度_mean'] + data_df.loc[i - 1, '辐照度_mean']) / 2
+        else:
+            data_df.loc[i, 'old_辐照度_mean'] = (data_df.loc[i, '辐照度_mean'] + data_df.loc[i - 1, '辐照度_mean'] + data_df.loc[
+                i - 2, '辐照度_mean']) / 3
     return data_df
 
 
