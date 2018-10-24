f9a0f1ab557af06ef43d438c4e436dd6925d4268
yanqiangmiffy <1185918903@qq.com>
2018-10-24 17:51:16 +0800

    delete others

--- utils.py
+++ utils.py
@@ -4,8 +4,11 @@
 # @File    : utils.py
 # @Software: PyCharm
 import pandas  as pd
+from sklearn.preprocessing import PolynomialFeatures
+
 
 def load_data():
+
     df_train_feature=pd.read_csv('input/train_feature.csv')
     df_train_label=pd.read_csv('input/train_label.csv')
     df_train_feature=df_train_feature.groupby(by='日期').mean().reset_index()
@@ -20,3 +23,63 @@ def load_data():
 
     return df_train,df_test
 
+
+df_train_feature=pd.read_csv('input/train_feature.csv')
+df_train_label=pd.read_csv('input/train_label.csv')
+df_test_feature = pd.read_csv('input/test_feature.csv')
+
+
+def to_one_day(df_data):
+
+    grouped=df_data.groupby('日期')
+    quarters=df_data['时刻'].drop_duplicates()
+    fea_cols=['辐照度','风速','风向','温度','湿度','气压']
+
+    day_col_names=['日期']
+    for qu in quarters:
+        for col in fea_cols:
+           day_col_names.append(str(qu)+'_'+col)
+    day_features=[]
+    for index,group in grouped:
+        temp=[index]
+        for i in range(group.shape[1]):
+            temp.extend(group[fea_cols].iloc[i].tolist())
+        day_features.append(temp)
+    df_day_data=pd.DataFrame(day_features,columns=day_col_names,index=None)
+    return df_day_data
+
+# df_day_data=to_one_day(df_train_feature)
+# print(df_day_data)
+
+
+def add_poly_features(data,column_names):
+    features=data[column_names]
+    rest_features=data.drop(column_names,axis=1)
+    poly_transformer=PolynomialFeatures(degree=2,interaction_only=False,include_bias=False)
+    poly_features=pd.DataFrame(poly_transformer.fit_transform(features),columns=poly_transformer.get_feature_names(column_names))
+
+    for col in poly_features.columns:
+        rest_features.insert(1,col,poly_features[col])
+    return rest_features
+
+
+def create_fea():
+    df_data=to_one_day(df_train_feature)
+    col_names = df_data.columns.tolist()[1:]
+    for i in range(6):
+        temp=[]
+        for j in range(i,len(col_names),6):
+            temp.append(col_names[j])
+        # print(df_data[temp])
+        df_data[temp[0].split('_')[1]+'_mean']=df_data[temp].mean(axis=1)
+        df_data[temp[0].split('_')[1]+'std']=df_data[temp].std(axis=1)
+        df_data[temp[0].split('_')[1]+'_min']=df_data[temp].min(axis=1)
+        df_data[temp[0].split('_')[1]+'_max']=df_data[temp].max(axis=1)
+
+    co = ['风速_mean', '辐照度_mean', '风向_mean', '温度_mean', '湿度_mean', '气压_mean']
+
+    df_data = add_poly_features(df_data, co)
+    print(df_data.columns)
+    return df_data
+
+create_fea()
